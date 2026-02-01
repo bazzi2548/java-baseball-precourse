ce573250bd826629112dbf8c3876661d680f8da2
문정환 <bazzi2548@naver.com>
2026-02-01 14:19:18 +0900

    feat: 스트라이크 및 볼 카운트 로직 구현

--- src/main/java/domain/BaseballNumbers.java
+++ src/main/java/domain/BaseballNumbers.java
@@ -3,6 +3,7 @@ package domain;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class BaseballNumbers {
 	private final List<Integer> numbers;
@@ -51,5 +52,38 @@ public class BaseballNumbers {
 		if (number < 1 || number > 9) {
 			throw new IllegalArgumentException("1에서 9 사이의 숫자만 입력 가능합니다.");
 		}
+	}
+
+	public int countStrike(BaseballNumbers other) {
+		int strike = 0;
+		for (int i = 0; i < numbers.size(); i++) {
+			strike += checkStrikeAt(other, i);
+		}
+		return strike;
+	}
+
+	private int checkStrikeAt(BaseballNumbers other, int index) {
+		if (this.numbers.get(index).equals(other.numbers.get(index))) {
+			return 1;
+		}
+		return 0;
+	}
+
+	public int countBall(BaseballNumbers other) {
+		int totalMatching = 0;
+		Set<Integer> computerSet = new HashSet<>(this.numbers);
+
+		for (int number : other.numbers) {
+			totalMatching += checkMatching(computerSet, number);
+		}
+
+		return totalMatching - countStrike(other);
+	}
+
+	private int checkMatching(Set<Integer> set, int number) {
+		if (set.contains(number)) {
+			return 1;
+		}
+		return 0;
 	}
 }
