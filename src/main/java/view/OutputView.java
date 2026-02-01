c8ce266fae8adca3b113b2f57e59b4c48e8fcc25
문정환 <bazzi2548@naver.com>
2026-02-01 15:34:41 +0900

    feat: 재시도 출력 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -3,7 +3,7 @@ package view;
 public class OutputView {
 	private static final String ERROR_PREFIX = "[ERROR] ";
 	private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
-	private static final String GAME_END_MESSAGE = "3스트라이크 \\n 3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+	private static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 	private static final String RESTART_GUIDE_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
 	public void printStartMessage() {
